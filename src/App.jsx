fd977e21b7a24c02c89c67f04b264bf1984d3c6d
Trinoy Kundu <trinoykundu@gmail.com>
2026-03-04 13:50:57 +0600

    style component with stylesheets

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Logo from "./Components/Logo";
+import './assets/global.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Learn with Sumit</h1>
+            <Logo />
+        </div>
+    );
+}
